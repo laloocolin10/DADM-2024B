e9e67b8e7f64787303aa6292f5ae114cfe457e3c
laloocolin10 <99770315+laloocolin10@users.noreply.github.com>
2024-09-23 18:55:39 +0000

    *ITEM: add a figure in this item

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
 import { ref } from 'vue'
 //modelo
 const header = ref('App lista de compras')
+const items = ref([
+  { id: '0', label: 'Aceite' },
+  { id: '1', label: 'Papel de baño' },
+  { id: '2', label: '1 cheve' },
+  { id: '3', label: '1 Pañal' },
+  { id: '4', label: '1 Nutella' }
+])
 </script>
 
 <template>
@@ -9,6 +16,9 @@ const header = ref('App lista de compras')
     <i class="material-icons shopping-cart-icon">local_mall</i>
     {{ header }}
   </h1>
+  <ul>
+    <li v-for="item in items" :key="item.id">CR7 {{ item.label }}</li>
+  </ul>
 </template>
 
 <style scoped>
