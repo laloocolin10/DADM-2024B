eba991b7a55dec020d567c49f10e32fbea680516
laloocolin10 <99770315+laloocolin10@users.noreply.github.com>
2024-09-23 18:18:53 +0000

    *FEAT: App Add a header and an Icon

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <script setup>
-
+import { ref } from 'vue'
+//modelo
+const header = ref('App lista de compras')
 </script>
 
 <template>
- <h1>
-  Shoping Cart
- </h1>
- </template>
- 
-<style scoped>
-</style> 
+  <h1>
+    <i class="material-icons shopping-cart-icon">local_mall</i>
+    {{ header }}
+  </h1>
+</template>
 
+<style scoped>
+.shopping-cart-icon {
+  font-size: 2rem;
+}
+</style>
