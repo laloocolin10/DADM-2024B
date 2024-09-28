e08d0b46dc761756b347d41cadf69ed86819dd67
laloocolin10 <99770315+laloocolin10@users.noreply.github.com>
2024-09-28 00:53:02 +0000

    *FEAT: Reto form Add an other botton for save other articules

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,10 @@ const items = ref([
   { id: '3', label: '1 Pañal' },
   { id: '4', label: '1 Nutella' }
 ])
+//realizado en clase
 const newItem = ref('')
 const newItemPriority = ref('low')
+
 </script>
 
 <template>
@@ -18,21 +20,35 @@ const newItemPriority = ref('low')
     <i class="material-icons shopping-cart-icon">local_mall</i>
     {{ header }}
   </h1>
+   <!--Realizado en clase -->
   <input v-model="newItem" type="next" placeholder="Agregar Articulo" /> {{ newItem }}
   <!--Radio Buttons-->
   <label>
-    <input type="radio" value="low" v-model0="newItemPriority" />
+    <input type="radio" value="low" v-model="newItemPriority" />
     Baja
   </label>
+
   <label>
-    <input type="radio" value="high" v-model0="newItemPriority" />
+    <input type="radio" value="high" v-model="newItemPriority" />
     Alta
   </label>
+
+  <!--Reto form -->
+  <form v-on:submit.prevent="items.push({ id: items.length + 1, label: newItem })" class="add-item fomr"> 
+    <button
+      class="btn btn-primary">
+      Salvar el articulo
+    </button>
+  </form>
+
   {{ newItemPriority == 'low' ? '🧊' : '🔥' }}
   <ul>
     <li v-for="item in items" :key="item.id">🛍️ {{ item.label }}</li>
   </ul>
 </template>
+
+
+
 
 <style scoped>
 .shopping-cart-icon {
