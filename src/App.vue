4afa3738b52753d3030c359056c4229307dbc0d4
laloocolin10 <99770315+laloocolin10@users.noreply.github.com>
2024-10-05 00:09:54 +0000

    *S13: HIPERLINK  add a vue link for condition for him

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,65 @@
 <script setup>
 import { ref } from 'vue'
-// Modelo
-const header = ref('App lista de compras')
-//items
-//item-model
+const header = ref('App Lista de compras')
+//---items---
+//Item.Model
 const items = ref([
-  { id: '0', label: '1 pepino' },
-  { id: '1', label: '1 Aceite para bebe' },
-  { id: '2', label: '1 barra de mantequilla' },
-  { id: '3', label: '1 nutella' }
-]);
-//item-Method
-const saveItem = () =>
-{
-   items.value.push({id: items.value.length + 1, label: newItem.value});
-   //clean the imput-Limpia el imput
-   newItem.value = '';
-};
-//visualizacion del formulario
-const newItem = ref('')
-const newItemHighPriority = ref(false);
-const editing = ref(false);
-const activateEdition = (activate) =>
-{
-  editing.value=activate;
-};
+  { id: 1, label: '1 Nutrileche' },
+  { id: 2, label: '1 Aceite de Bebe' },
+  { id: 3, label: '1 lata de atún' },
+  { id: 4, label: '1 Nutella' }
+])
 
+// Items-Method
+const saveItem = (items) => {
+  items.value.push({ id: items.value.length + 1, label: newItem.value })
+  //Clean the input
+  newItem.value = ''
+}
+
+// Visualizacion formulario
+const newItem = ref('')
+const newItemHighPriority = ref(false)
+const editing = ref(true)
+const activeteEdition = (activate) => {
+  editing.value = activate
+}
 </script>
 
 <template>
-  <div>
-  <h1>
-    <i class="material-icons shopping-cart-icon">local_mall
-
-    </i>
-    {{ header }}
-  </h1>
-  <button v-if="editing" class="btn" @click="activateEdition(false)">Cancelar</button>
-  <button v-else class="btn btn-primary" @click="activateEdition(true)">Agregar Articulo</button>
+  <div class="header">
+    <h1>
+      <i class="material-icons shopping-cart-icon"> local_mall </i>
+      {{ header }}
+    </h1>
+    <button v-if="editing" class="btn" @click="activeteEdition(false)">Cancelar</button>
+    <button v-else class="btn btn-primary" @click="activeteEdition(true)">Agregar</button>
   </div>
 
-  <form
-    class="add-item form"
-    v-on:submit.prevent="saveItem"
-    v-if="editing"
-    
-  >
-    <input v-model="newItem" type="text" placeholder="Agregar un articulo" />
-    <!--Caja de seleccion de Prioridad-->
+  <!--colocando un hiperlink visto en clase-->
+  <a v-bind:href="newItem === '' ? 'https://www.google.com' : 'https://' + newItem" target="_blank">
+    {{ newItem === '' ? '🖇Link' : newItem }}
+  </a>
 
-    <label>
-      <input type="checkbox" v-model="newItemHighPriority" />
-      Alta Prioridad
-    </label>
 
-    <!--Boton-->
+  <!-- Agrupando Entradas de usuario -->
+  <form class="add-item form" v-if="editing" v-on:submit.prevent="saveItem">
+    <!-- Entrada de texto -->
+    <input type="text" placeholder="Add Item" v-model.trim="newItem" />
+    <!-- Radio Buttons -->
+    <label><input type="checkbox" v-model="newItemHighPriority" />Alta Prioridad</label>
+    <!-- Boton -->
     <button class="btn btn-primary">Salvar Articulo</button>
   </form>
-
-  {{ newItemHighPriority }}
   <!-- Lista -->
   <ul>
-    <li v-for="item in items" :key="item.id">𓅓 {{ item.label }}</li>
+    <li v-for="{ id, label } in items" v-bind:key="id">𓅓 {{ label }}</li>
   </ul>
-  <p v-if="items.length === 0">🥀 NO HAY ELEMENTOS DE TU LISTA 🥀</p>
+  <p v-if="items.length === 0">🥀 NO HAY ELEMENTOS EN LA LISTA 🥀</p>
 </template>
 
 <style scoped>
 .shopping-cart-icon {
-  font-size: 2rem;
+  font-size: 2rem; /* Adjust the font-size value as per your desired size*/
 }
 </style>
