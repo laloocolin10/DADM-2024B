c75804c6a3622788873b42fbf4825f0d2cf93eba
laloocolin10 <99770315+laloocolin10@users.noreply.github.com>
2024-11-28 00:13:05 +0000

    🚧S03S07 onMounted

--- src/components/PlanPicker.vue
+++ src/components/PlanPicker.vue
@@ -1,27 +1,41 @@
 <script setup>
-  import PlanPickerItem from './PlanPickerItem.vue';
-  import { ref } from 'vue';
-  //Creando una referencia reactiva para almacenar el plan deseado 
-  const selectedPlan=ref(null)
-  const plans = ref(['El cafetero', 'El chelero', 'El intelectual']);
-  const selectPlan = (name) => {
-    //Almacenar el plan seleccionado
-    selectedPlan.value =name; 
-  };
+import PlanPickerItem from './PlanPickerItem.vue'
+import { ref, onMounted, onUnmounted } from 'vue'
+/*Creando una variable reactiva para 
+almacenar el plan seleccionado*/
+const selectedPlan = ref(null)
+
+const plans = ref(['El cafetero', 'El chelero', 'El intelectual'])
+const selectPLan = (name) => {
+  //alamecenar el plan almacenado
+  selectedPlan.value = name
+}
+
+const plansWrapper = ref(null)
+//intentando obtener la referencia al elemento .plans
+console.log('termina de ejecutar el scrip setup')
+
+//intentando obtener la referencia al elemento .plans
+onMounted(() => {
+  console.log('El componente PlanPicker fue montado 😎')
+  console.log(plansWrapper.value)
+})
+//registrando el hook mounted
+onUnmounted(() => {
+  console.log('El componente PlanPicker se ha desmontado')
+})
 </script>
 
 <template>
-    <div class="plans">
-      <PlanPickerItem 
-      v-for="plan in plans" 
+  <div ref="plansWrapper" class="plans">
+    <PlanPickerItem
+      v-for="plan in plans"
       :key="plan"
       :name="plan"
-      @select="selectPlan"
-      :select-plan="selectedPlan"/>
-    </div>
-    
+      @select="selectPLan"
+      :select-plan="selectedPlan"
+    />
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
